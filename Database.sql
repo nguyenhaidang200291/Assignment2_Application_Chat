1908e00a2391125354830212fa57f64306e61ef0
nguyenhaidang200291 <nguyenhaidang200291@gmail.com.vn>
2020-09-15 23:44:56 +0700

    update

---
+++ Database.sql
@@ -0,0 +1,19 @@
+INSERT INTO roles (id, name) VALUES 
+(1, 'ROLE_ADMIN'),
+(2, 'ROLE_ACTUATOR'),
+(3, 'ROLE_USER')
+;
+
+INSERT INTO users (id, email, password, name) VALUES 
+(1, 'admin@fsoft.com.vn', '$2a$10$hKDVYxLefVHV/vtuPhWD3OigtRyOykRLDdUAp80Z1crSoS1lFqaFS', 'Admin'),
+(2, 'dangnh5@fsoft.com.vn', '$2a$10$ByIUiNaRfBKSV6urZoBBxe4UbJ/sS6u1ZaPORHF9AtNWAuVPVz1by', 'User'),
+(3, 'thainc@fsoft.com.vn', '$2a$10$ByIUiNaRfBKSV6urZoBBxe4UbJ/sS6u1ZaPORHF9AtNWAuVPVz1by', 'User');
+
+
+insert into user_role(user_id, role_id) values
+(1,1),
+(1,2),
+(1,3),
+(2,3),
+(3,2)
+;
