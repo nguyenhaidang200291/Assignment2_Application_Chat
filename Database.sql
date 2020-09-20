e7cf4aed70bf35ab74069492ec8e0478b52ddb43
nguyenhaidang200291 <nguyenhaidang200291@gmail.com.vn>
2020-09-21 00:35:59 +0700

    update

--- Database.sql
+++ Database.sql
@@ -6,8 +6,8 @@ INSERT INTO roles (id, name) VALUES
 
 INSERT INTO users (id, email, password, name) VALUES 
 (1, 'admin@gmail.com.vn', '$2a$10$hKDVYxLefVHV/vtuPhWD3OigtRyOykRLDdUAp80Z1crSoS1lFqaFS', 'Admin'),
-(2, 'dangnh5@gmail.com.vn', '$2a$10$ByIUiNaRfBKSV6urZoBBxe4UbJ/sS6u1ZaPORHF9AtNWAuVPVz1by', 'User'),
-(3, 'thainc@gmail.com.vn', '$2a$10$ByIUiNaRfBKSV6urZoBBxe4UbJ/sS6u1ZaPORHF9AtNWAuVPVz1by', 'User');
+(2, 'nguyenhaidang@gmail.com.vn', '$2a$10$ByIUiNaRfBKSV6urZoBBxe4UbJ/sS6u1ZaPORHF9AtNWAuVPVz1by', 'User'),
+(3, 'nguyencongthai@gmail.com.vn', '$2a$10$ByIUiNaRfBKSV6urZoBBxe4UbJ/sS6u1ZaPORHF9AtNWAuVPVz1by', 'User');
 
 
 insert into user_role(user_id, role_id) values
